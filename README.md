1401ca68f5f7b9306b41c2a85d69f105b724c7e5
Rich Jen <rich.rjen@gmail.com>
2019-04-01 15:36:20 -0400

    creating newsscraper
    initial commit of news scraper
    all files needed for mvp

---
+++ README.md
@@ -0,0 +1,30 @@
+# Fit for Scraping Node App
+
+This app is an example scraper I built in node to show how to scrape a simple news website.
+
+
+## Prerequisites:
+1. Setup mongo db server
+
+
+## Setup Instructions:
+
+1. clone repo
+2. npm install
+3. run index.js
+
+
+## Instructions:
+
+1. After the app is running, go to the site `/scrape`
+
+1. After you see the success message, go back to `/`
+
+1. Click on any of the article location to select the note box to take notes
+
+1. Click save to save the notes to the database, you can remove notes as well and click save it wil just overwrite and display the most recent changes.
+
+1. Click on the url to leave the site and visit the news article.
+
+### Additional Notes:
+Link to deployed application on heroku: [Link](https://dry-dusk-84895.herokuapp.com/)
---
+++ index.js
@@ -0,0 +1,76 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var db = require("./models");
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/newsScrape", { useNewUrlParser: true });
+
+
+app.get("/scrape", function(req, res) {
+  axios.get("https://www.aljazeera.com/topics/regions/us-canada.html").then(function(response) {
+    var $ = cheerio.load(response.data);
+
+    $(".topics-sec-item").each(function(i, element) {
+      var result = {};
+
+      result.title = $(this).find('.topics-sec-item-cont').find('.topics-sec-item-head').text();
+      result.link = 'https://www.aljazeera.com' + $(this).find('.topics-sec-item-cont').find('a').attr('href');
+      result.summary = $(this).find('.topics-sec-item-cont').find('.topics-sec-item-p').text();
+
+      db.Article.create(result).then(function(dbArticle) {
+      }).catch(function(err) {
+          console.log(err);
+      })
+    });
+
+    res.send("Scrape Complete");
+  });
+});
+
+app.get("/articles", function(req, res) {
+  db.Article.find({})
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.get("/articles/:id", function(req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT);
+});
